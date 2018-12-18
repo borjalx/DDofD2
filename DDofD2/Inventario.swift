e90385eeb82b95fb04919e163a67af841e91721b
Borja S <borjas@ot-triciclo.local>
2018-12-18 19:54:15 +0100

    Inventario "HECHO", "HACIENDO" Tienda : comprar ítems, mostrar dinero actual y (opcional)comparación de ítems

--- DDofD2/Inventario.swift
+++ DDofD2/Inventario.swift
@@ -11,9 +11,10 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
     @IBOutlet weak var itemsPV: UIPickerView!
     @IBOutlet weak var lblMensajes: UILabel!
     
+    let arrayItems: [Item] = [(heroina.equipacion?.arma)!,(heroina.equipacion?.armadura)!,(heroina.equipacion?.casco)!,(heroina.equipacion?.escudo)!,(heroina.equipacion?.botas)!]
     
     //Auxiliares
-    var itemActual: Item = Item()
+    //var itemActual: Item = Item()
     
     
     override func viewDidLoad() {
@@ -32,7 +33,7 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return heroina.inventario.count
+        return arrayItems.count
     }
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
@@ -45,13 +46,15 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         let magiaItem: UILabel = UILabel(frame: CGRect(x: 30, y: 60, width: 150, height: 100))
         let suerteItem: UILabel = UILabel(frame: CGRect(x: 30, y: 80, width: 150, height: 100))
         
+        
+        
         imagen.image = heroina.inventario[row].imagen!
-        nombreItem.text = "Nombre : \(heroina.inventario[row].nombre)"
-        tipoItem.text = "Tipo : \(heroina.inventario[row].tipo)"
-        ataqueItem.text = "Ataque : \(heroina.inventario[row].ataque)"
-        defensaItem.text = "Defensa : \(heroina.inventario[row].defensa)"
-        magiaItem.text = "Magia : \(heroina.inventario[row].magia)"
-        suerteItem.text = "Suerte : \(heroina.inventario[row].suerte)"
+        nombreItem.text = "Nombre : \(arrayItems[row].nombre)"
+        tipoItem.text = "Tipo : \(arrayItems[row].tipo)"
+        ataqueItem.text = "Ataque : \(arrayItems[row].ataque)"
+        defensaItem.text = "Defensa : \(arrayItems[row].defensa)"
+        magiaItem.text = "Magia : \(arrayItems[row].magia)"
+        suerteItem.text = "Suerte : \(arrayItems[row].suerte)"
         
         
         vista.addSubview(imagen)
@@ -61,8 +64,7 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         vista.addSubview(defensaItem)
         vista.addSubview(magiaItem)
         vista.addSubview(suerteItem)
-        
-        
+
         
         return vista
     }
@@ -70,10 +72,14 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
         return 170
     }
-    
+    /*
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         //Heroina auxiliar con los mismos datos que la heroina actual
         let hAux:Heroe = heroina
+        
+        //Array auxiliar de los items
+        
+        
         //Susitituimos el item actual por el de la equipación
         switch(heroina.inventario[row].tipo){
             case "arma":
@@ -108,11 +114,12 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         ahSuerte.textColor = getColor(numeroActual: heroina.suerte, numeroNuevo: hAux.suerte)
         
     }
-
+    */
+    
     //Funciones
     
     //Función que devulve el color () de un texto dependiendo de si es mejor o peor
-    func getColor(numeroActual: Int, numeroNuevo: Int) -> UIColor {
+    /*func getColor(numeroActual: Int, numeroNuevo: Int) -> UIColor {
         //Comparamos dos números
         //Si el primero es menor al segundo devulve el color rojo
         //Si los números son iguales devuelve el color blanco
@@ -129,6 +136,7 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         
         return colorFinal
     }
+     */
     
     //Función que carga los valors de los objetos iniciales
     func cargaInicial(){
@@ -149,7 +157,7 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
     //TO DO - Utilizar los valores de los items del PV y compararlos con los actuales y establecer como itemActual
     
     //Sustituir el item del PV por el de la equipación actual
-    @IBAction func utilizarItem(_ sender: Any) {
+    /*@IBAction func utilizarItem(_ sender: Any) {
         //Buscamos el item por el que sustituiremos
         //Sustituimos el item de la equipación por el del PV
         //Avisamos al cliente con un mensaje
@@ -183,4 +191,5 @@ class Inventario: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate
         }
         
     }
+     */
 }
